15b50fbffa304130fb34a1df43f6d9324c6c8ce3
jomicm <singularity.jmcm@gmail.com>
2019-11-11 19:37:09 -0500

    Update excercise_1

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,12 @@
 require_relative '../setup'
 require_relative './exercise_1'
+require_relative '../lib/store'
 
 puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+@store1 = Store.where(:id => 1)
+@store2 = Store.where(:id => 2)
+
+puts @store1[0][:name]
